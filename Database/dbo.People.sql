46eb1a8e08c0475bc99c863a6233afc79d562265
Arik Jones <tnypxl00@icloud.com>
2020-08-01 15:43:14 -0500

    Completed BlazorDemoApp

---
+++ Database/dbo.People.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].People
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARBINARY(MAX) NULL, 
+    [LastName] VARBINARY(MAX) NULL, 
+    [DateOfBirth] DATE NULL, 
+    [EmailAddress] VARCHAR(MAX) NULL
+)
