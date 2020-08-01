52241509bb45361ab1618dee5373dafeb865a356
root <root@DESKTOP-RESF9CI.localdomain>
2020-08-01 15:48:12 -0500

    Completed BlazorDemoApp

--- DataAccessLibrary/Models/PersonModel.cs
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataAccessLibrary.Models
+﻿namespace DataAccessLibrary.Models
 {
     public class PersonModel
     {
